e83e780725d9f4ff3dd93b26b47d9fb910b18472
Anton Sitnikov <anton.sitnikov@exactprosystems.com>
2018-12-20 19:09:48 +0300

    Add license; Fix copyright years

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 '''
 /*******************************************************************************
-* Copyright 2009-2018 Exactpro (Exactpro Systems Limited)
+* Copyright 2016-2018 Exactpro (Exactpro Systems Limited)
 * 
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
