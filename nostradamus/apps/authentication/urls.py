7ba279d29608095b0fa19638e2cc498f783ffe90
svyat935 <svyat935@mail.ru>
2021-07-10 21:54:46 +0300

    release-8.1.0

--- nostradamus/apps/authentication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import AuthenticationView, RegistrationView
-
-urlpatterns = [
-    path("signin/", AuthenticationView.as_view()),
-    path("register/", RegistrationView.as_view()),
-]
